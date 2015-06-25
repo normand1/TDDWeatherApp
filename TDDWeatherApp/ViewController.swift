302f27a778a1d2d7b0a501213f8330b519571d44
David Norman <david.norman.w@gmail.com>
2015-06-25 10:33:28 -0700

    Initial Commit

---
+++ TDDWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDDWeatherApp
+//
+//  Created by David Norman on 6/25/15.
+//  Copyright (c) 2015 David Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
